afce52caf4c22503a65e554c50410561e217d77b
Li Tao <gujie713@gmail.com>
2022-09-29 23:08:14 +1000

    added user crud

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -12,15 +12,24 @@ users = Blueprint("users", __name__, url_prefix="/users")
 @users.route("/", methods=["GET", "PUT"])
 @jwt_required()
 @authenticate_role_only(role="user")
-def get_current_user():
-    if request.method == "GET":
-        user = Barista.query.get(get_jwt_identity().replace("user", ""))
-        if not user:
-            return {"ERROR": "User does not exist."}, 400
+def get_update_user():
+    user = Barista.query.get(get_jwt_identity().replace("user", ""))
 
+    if not user:
+        return {"ERROR": "User does not exist."}, 400
+
+    if request.method == "GET":
         return jsonify(barista_schema.dump(user))
 
     else:
-        barista_fields = barista_schema.load(request.json)
+        barista_fields = barista_schema.load(request.json, partial=True)
 
-        print(barista_fields)
+        user.first_name = barista_fields.get("first_name", user.first_name)
+        user.last_name = barista_fields.get("last_name", user.last_name)
+        user.email = barista_fields.get("email", user.email)
+        user.contact_number = barista_fields.get("contact_number", user.contact_number)
+        user.address = barista_fields.get("address", user.address)
+
+        db.session.commit()
+
+        return jsonify(barista_schema.dump(user))
